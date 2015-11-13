6efaf75da1f316b7b2cbc420f6fb6d71d14f5cd9
Esteban Torres <EstebanTorres@estebans-macbook-pro-2.local>
2015-11-13 16:48:29 -0600

    Initial Commit

---
+++ TV Alert/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TV Alert
+//
+//  Created by Esteban Torres Alarcon on 11/13/15.
+//  Copyright © 2015 CeroUno. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
